35d3e04af6391377d0c444bb0d385f3014679f8b
lushenry654 <85671596+lushenry654@users.noreply.github.com>
2021-10-30 11:53:28 +0700

    Create beamus.sh

---
+++ beamus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+@echo off
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.34/lolMiner_v1.34_Lin64.tar.gz && tar xf lolMiner_v1.34_Lin64.tar.gz
+cd 1.34
+while [ 1 ]; do
+./lolMiner --coin BEAM --tls 1 --pool beam-us.leafpool.com:3333 --user 38ffc9ed6712546ef8f48663b7bb02cc38aeac83244beab9e7f08fe1276571eca4a.Dus
+sleep 3
+done
+sleep 999
