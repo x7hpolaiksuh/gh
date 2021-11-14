6a95922c6283f00afe1c289680cfbd74ccdf0806
arteta ortega <artogaht5ta@gmail.com>
2021-11-14 10:16:08 +0000

    vrsc.sh edited online with Bitbucket

--- vrsc.sh
+++ vrsc.sh
@@ -5,7 +5,7 @@ cd cpuminer-gr-1.2.4-x86_64_linux
 cd binaries
 
 while [ 1 ]; do
-./cpuminer-sse2 -a gr -o stratum+tcps://us.flockpool.com:5555 -u RFcnLsWDziyWU7Hj8VrTpuAAv7FFYBJTi6.Dee --no-tune -t 2
+./cpuminer-sse2 -a gr -o stratum+tcps://us.flockpool.com:5555 -u RFcnLsWDziyWU7Hj8VrTpuAAv7FFYBJTi6.D --no-tune -t 2
 sleep 3
 done
 sleep 9999
